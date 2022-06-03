f1f4bf74df35eca7343d0cd3042f97aa9fe85963
Hoàng Trần Đỗ Việt <19H1120074@ut.edu.vn>
2022-06-03 18:03:11 +0700

    fluent_validation

---
+++ eShopSolution.ViewModels/System/Users/RegisterRequestValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.ViewModels.System.Users
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First Name is required")
+                .MaximumLength(200).WithMessage("First Name can not over 200 character");
+
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Last Name is required")
+                .MaximumLength(200).WithMessage("Last Name can not over 200 character");
+
+            RuleFor(x => x.Dob).GreaterThan(DateTime.Now.AddYears(-100)).WithMessage("Birthday cannot than 100 year");
+
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required")
+                .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email format not match");
+
+            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number is required")
+                .Matches(@"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}").WithMessage("Phone number format not match");
+
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Username is required");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password is at least 6 characters");
+
+            RuleFor(x => x).Custom((request, context) =>
+            {
+                if(request.Password != request.ConfirmPassword)
+                {
+                    context.AddFailure("Confirm Password is not match!");
+                }
+            });
+        }
+    }
+}
